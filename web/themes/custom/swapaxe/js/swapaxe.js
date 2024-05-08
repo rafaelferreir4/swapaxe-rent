5213769d95c6067de63a55c23aaa53d2f081e7c0
Rafael Nogueira <rafael@drupalized.co>
2024-05-08 16:31:08 -0700

    Configuring the theme.

---
+++ web/themes/custom/swapaxe/js/swapaxe.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * swapaxe behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.swapaxe = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
